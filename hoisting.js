18aed263688d6bcc90e9361f877a59c40b64470b
Emma Nam <en2458@gmail.com>
2020-11-14 04:28:51 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,25 +1,15 @@
+// variable hoisting
+
+var maybe = 'maybe';
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  console.log("maybe");
-  var lyric = "maybe";
-  return lyric;
+  return 'maybe'
 }
 
-function crazy() {
-  console.log("hey!!!")
-  var thisIsCrazy = function (){
-    thisIsCrazy();
-  }
-}
+//function hoisting
 
-function sayMyName() {
-  sayMy();
-  function sayMy() {
-    console.log (name);
-    var name = "Kristin";
-    console.log (name);
-  }
+var crazy = 'hey!!!'
+
+function thisIsCrazy() {
+  console.log('hey!!!')
 }
+thisIsCrazy();
